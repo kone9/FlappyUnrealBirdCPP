f7d75e8bca249a62d56368f72c681067a45a6ce8
Ariel Gimenez <arielgimenez_95@hotmail.com>
2022-09-19 11:03:19 -0300

    agregando game instance con puntaje y detalle para iniciar transicion en primer nivel

---
+++ Source/FlappyUnrealBirdCPP/Public/GameInstance_FlappyUnrealBird.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstance_FlappyUnrealBird.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYUNREALBIRDCPP_API UGameInstance_FlappyUnrealBird : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int max_score{ 15 };
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool start_game_once{ false };
+
+};
