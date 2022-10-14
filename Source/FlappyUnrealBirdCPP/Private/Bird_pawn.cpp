b1f39cb1fb2feb44446701a1edcaf216f572f940
Ariel Gimenez <arielgimenez_95@hotmail.com>
2022-10-14 12:13:04 -0300

    sistema de vidas y score entre niveles listo, pero tengo bugs en el score

--- Source/FlappyUnrealBirdCPP/Private/Bird_pawn.cpp
+++ Source/FlappyUnrealBirdCPP/Private/Bird_pawn.cpp
@@ -365,7 +365,7 @@ int ABird_pawn::add_score()
 
 	UGameplayStatics::PlaySound2D(GetWorld(), coint_sound);
 
-	game_mode->actual_score += 1;
+	game_mode->actual_score = game_mode->actual_score + 1;
 
 	int score = game_mode->actual_score;
 	FString score_String{ FString::FromInt(score) };
